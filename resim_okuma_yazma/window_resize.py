40e232ca4a9039ca81680c6511c91e7f988da56a
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-10-22 23:26:06 +0300

    clear

--- resim_okuma_yazma/window_resize.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-cv2.namedWindow("resim", cv2.WINDOW_NORMAL)
-img = cv2.imread("resim.jpg")
-img = cv2.resize(img, (640, 480))
-
-cv2.imshow("resim", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
